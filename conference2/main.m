c81825f652c9a159d5623ef6173af2b34441a318
Jon <jon@jonathan-wongs-macbook-pro.local>
2011-12-14 16:40:12 -0800

    Initial Commit

---
+++ conference2/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  conference2
+//
+//  Created by Jonathan Wong on 12/14/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "cnfAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([cnfAppDelegate class]));
+    }
+}
